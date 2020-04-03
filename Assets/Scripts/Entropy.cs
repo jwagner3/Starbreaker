c2a86eeb1675c4585062b60728270d524c00156a
Jackson Wagner <jwagner3@stedwards.edu>
2020-04-03 17:18:24 -0500

    First try at level design, added skybox, first whack at entropy monster look, jetpack ability implemented

---
+++ Assets/Scripts/Entropy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entropy : MonoBehaviour
+{
+
+    public GameObject player;
+    public float moveSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.LookAt(player.transform.position);
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}
